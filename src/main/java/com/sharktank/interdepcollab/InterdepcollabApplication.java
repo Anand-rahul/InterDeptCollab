0d2984e1ef91f275c4cdf8cdf01f13fdc0a869d5
Shoumik <shoumikrp2000@gmail.com>
2025-02-10 11:14:33 +0000

    Spring Initialised

---
+++ src/main/java/com/sharktank/interdepcollab/InterdepcollabApplication.java
@@ -0,0 +1,13 @@
+package com.sharktank.interdepcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterdepcollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterdepcollabApplication.class, args);
+	}
+
+}
